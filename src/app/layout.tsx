2103ff89bae3991b86f4bce16c730b3e987fe5d8
vedant-dev <vedantdbhamare@gmail.com>
2025-03-31 15:46:18 +0530

    Implemented REdux into project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+"use client";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Provider } from "react-redux";
+import store from "@/redux/store";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,10 +15,6 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Vedant Portfolio",
-  description: "This is the Portfolio website of Vedant",
-};
 
 export default function RootLayout({
   children,
@@ -23,6 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <Provider store={store}>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -30,5 +30,6 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+    </Provider>
   );
 }
