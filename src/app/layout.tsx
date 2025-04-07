2f7db6aa47d4f82163f30aa6ef72aaf5304a8d4d
vedant-dev <vedantdbhamare@gmail.com>
2025-04-07 11:00:33 +0530

    enebled vercel alalytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ProviderWrapper from "@/components/ProviderWrapper"; // Import ProviderWrapper
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           {children}
+          <Analytics />
         </body>
       </html>
     </ProviderWrapper>
